6ffa3b40d9206d7cd3d77541a37b55a4b26c4eb2
RojanOmidvar <46855885+RojanOmidvar@users.noreply.github.com>
2019-03-20 15:38:12 -0300

    Update Distribution.java

--- Distribution.java
+++ Distribution.java
@@ -9,18 +9,28 @@ public abstract class Distribution {
 	private Map<String, ArrayList<Course>> distributionMap;
 	private GradeSchema gradeSchema;
 	
-/*
+	static enum Level {
+		Exceeds,
+		Meets,
+		Marginal,
+		Fails,
+		Other
+        }
+	
 	public Distribution(String gradeSchemaFile) {
-		distributionMap = new HashMap<String, ArrayList<Course>>();
 		gradeSchema = new GradeSchema(gradeSchemaFile);
+		distributionMap = new HashMap<String, ArrayList<Course>>();
 	}
-*/
 	
 	public Map<String, Integer> createDistributionMap(){
 		Map<String, Integer> distributionMap = new HashMap<String, Integer>();
+		Set<Level> levels = gradeSchema.getLevels();
+		for (Level level : levels){
+			distributionMap.put(level, 0);
+		}
 		return distributionMap;
 	}
-	
+
 	public ArrayList<String> getLetterGrades(ArrayList<Course> courseList){
 		ArrayList<String> letterGrades = new ArrayList<String>();
 		for (Course course : courseList) {
