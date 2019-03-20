3bc2adf44fa3b9054f4366de630e80270f4a09df
RojanOmidvar <46855885+RojanOmidvar@users.noreply.github.com>
2019-03-20 16:07:13 -0300

    Update AreaDistribution.java

--- AreaDistribution.java
+++ AreaDistribution.java
@@ -7,19 +7,32 @@ import java.util.HashMap;
 
 public class AreaDistribution extends Distribution{
 
-	String area;
-	Map<String, Integer> areaDistributionMap;
+	ArrayList<String> areas;
+	Map<String, Map<Level, Integer>> areaDistributionMap;
 	
-	public AreaDistribution(String area, ArrayList<Course> courseList) {
-		areaDistributionMap = this.createDistributionMap();
-		
-/*
-		for(Course course : courseList) {
-			String letterGrade = course.getGrade().getLetterGrade();
-			for(String level : areaDistributionMap) {
+	public AreaDistribution(String gradeSchemaFile) {
+		Map<String, Map<Level, Integer>> areaDistributionMap = new HashMap<String, Map<Level, Integer>>();
+		areas = AreaSchema.getAllAreas();
+		GradeSchema gradeSchema = new GradeSchema(gradeSchemaFile);
+		for (String area : areas){
+			this.setDistributionForArea(area);	
+		}
+	}
+
+	public void setDistributionForArea(String areaName) {
+		Map<String, Integer> areaDistribution = this.createDistributionMap();
+		ArrayList<Course> coursesOfName = courseNameMap.get(courseName);
+		ArrayList<String> letterGrades = this.getLetterGrades(coursesOfName);
+		for (String letterGrade : letterGrades){
+			Set<Level> levels = gradeSchema.getLevels();
+			for (Level level : levels){
+				Set<String> gradesInLevel = gradeSchema.getGradesForLevel();
+				if (gradesInLevel.contains(letterGrade){
+					Integer number = courseDistribution.get(level.toString())
+					courseDistribution.set(level.toString(), number++);
+				}
 			}
 		}
-*/
-
+		areaDistributionMap.put(areaName, areaDistribution);
 	}
 }
