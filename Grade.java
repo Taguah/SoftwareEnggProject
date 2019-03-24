f19474c69f30f901bd33983c52dd04efd4112f29
RojanOmidvar <46855885+RojanOmidvar@users.noreply.github.com>
2019-03-24 04:00:49 -0300

    Update Grade.java

--- Grade.java
+++ Grade.java
@@ -29,6 +29,24 @@ public class Grade {
 		return numberGrade;
 	}
 	
+	public String convertNumberToLetter(double number){
+		String letterGrade;
+		switch(number) {
+			case 4.0: letterGrade = "A";
+			case 3.7: letterGrade = "A-";
+			case 3.3: letterGrade = "B+";
+			case 3.0: letterGrade = "B";
+			case 2.7: letterGrade = "B-";
+			case 2.3: letterGrade = "C+";
+			case 2.0: letterGrade = "C";
+			case 1.7: letterGrade = "C-";
+			case 1.3: letterGrade = "D+";
+			case 1.0: letterGrade = "D";
+			default: letterGrade = "D-";
+		}
+		return letterGrade;
+	}
+	
 	public double getNumberGrade() {
 		return numberGrade;
 	}
