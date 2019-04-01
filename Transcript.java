b91123804524e2d63388505cfd8fb9271cc6c109
TedCamp <47256211+TedCamp@users.noreply.github.com>
2019-04-01 17:52:19 -0300

    Update Transcript.java
    To fix the retaking courses issue

--- Transcript.java
+++ Transcript.java
@@ -1,7 +1,7 @@
-package project.excelSpike;
+package project.excelSpike2;
 
 /*
-@author Jenny Wang
+@author Jenny Wang, Ted Camp
 */
 
 import java.util.List;
@@ -9,36 +9,43 @@ import java.util.ArrayList;
 
 public class Transcript {
 
-    private ArrayList<Course> courselist;
+    private CourseList courseList;
 
     public Transcript() {
-        courselist = new ArrayList<Course>();
+        this.courselist = new CourseList();
     }
-   
+
     public void addCourse(Course course) {
-    	this.courselist.add(course);
+    	this.courseList.add(course);
     }
-    
+
     public Transcript(ArrayList<Course> courseListIn) {
-        courselist = courseListIn;
+    	this.courselist = new CourseList();
+    	for (Course course : courseListIn) {
+        	courseList.addCourse(course);
+        }
     }
 
-    public ArrayList<Course> getCourses() {
-        return courselist;
-    }
-
-    public ArrayList<Grade> getAllGrades(){
-    	ArrayList<Grade> grades = new ArrayList<Grade>();
-    	for(Course course : courselist) {
-    		grades.add(course.getGrade());
-    	}
-    	return grades;
+    public List<Course> getCourses() {
+    	List<Course> courses = courseList.getCourses();
+        return courses;
     }
     
+    public List<Course> getPassingCourses() {
+    	List<Course> courses = courseList.getPassingCourses();
+        return courses;
+    }
+
+    public List<Course> getRetakenCourses() {
+    	List<Course> courses = courseList.getRetakenCourses();
+        return courses;
+    }
+
     public List<Course> getCoursesByArea(String area){
     	List<Course> areaCourses = new ArrayList<Course>();
     	List<String> relevantCourses = AreaSchema.getAllCoursesInArea(area);
-    	for (Course course : courselist) {
+    	List<Course> courses = getCourses();
+    	for (Course course : courses) {
     		String courseName = course.getCourseID();
     		courseName = EquivalencySchema.getEquivalent(courseName);
     		if (relevantCourses.contains(courseName)){
@@ -47,13 +54,27 @@ public class Transcript {
     	}
     	return areaCourses;
     }
-
+    
+    public List<Course> getRetakenCoursesByArea(String area){
+    	List<Course> areaCourses = new ArrayList<Course>();
+    	List<String> relevantCourses = AreaSchema.getAllCoursesInArea(area);
+    	List<Course> courses = getRetakenCourses();
+    	for (Course course : courses) {
+    		String courseName = course.getCourseID();
+    		courseName = EquivalencySchema.getEquivalent(courseName);
+    		if (relevantCourses.contains(courseName)){
+    			areaCourses.add(course);
+    		}
+    	}
+    	return areaCourses;
+    }
     public double getAverageForArea(String area){
         double average = 0;
         double creditHours = 0;
         
         List<String> coursesInArea = AreaSchema.getAllCoursesInArea(area);
-        for (Course course : courselist) {
+        List<Course> courses = getCourses();
+        for (Course course : courses) {
         	if (coursesInArea.contains(course.getCourseID())) {
         		creditHours += course.getCreditHours();
         		average += course.getCreditHours() * ( course.getGrade().getNumberGrade() );
@@ -67,4 +88,17 @@ public class Transcript {
         }
     }
     
+    public String checkRank() {
+    	double totalCreditHours = 0;
+    	//Check retaken needs to be added
+    	List<String> coursesTaken = new ArrayList<String>();
+    	List<Course> courses = courseList.getPassingCourses();
+    	for (Course course : courses) {
+    		totalCreditHours = totalCreditHours + course.getCreditHours();
+    	}
+    	//Below needs rank schema
+    	//String rank = RankSchema.checkRank(totalCreditHours);
+    	String rank = "Boop";
+    	return rank;
+    }
 }
